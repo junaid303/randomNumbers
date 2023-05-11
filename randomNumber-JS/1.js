f66e475563629aa205a0e5663bbd6c0f7b970f07
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-05-11 21:34:11 +0530

    generating random in JS

---
+++ randomNumber-JS/1.js
@@ -0,0 +1,23 @@
+// Returns a random integer from 0 to 9:
+var data1 = Math.floor(Math.random() * 10);
+console.log(data1);
+
+// Returns a random integer from 0 to 10:
+var data2 = Math.floor(Math.random() * 11);
+console.log(data2);
+
+// Returns a random integer from 0 to 99:
+var data3 = Math.floor(Math.random() * 100);
+console.log(data3);
+
+// Returns a random integer from 0 to 100:
+var data4 = Math.floor(Math.random() * 101);
+console.log(data4);
+
+// Returns a random integer from 1 to 10:
+var data5 = Math.floor(Math.random() * 10) + 1;
+console.log(data5);
+
+// Returns a random integer from 1 to 100:
+var data6 = Math.floor(Math.random() * 100) + 1;
+console.log(data6);
